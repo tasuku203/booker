1453d99af53e76860f303a91232a71ed634a5ba4
EC2 Default User <ec2-user@ip-172-31-90-29.ec2.internal>
2022-01-09 09:44:11 +0000

    [Update]3-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    
+  end
+
+  def show
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+  end
+end
